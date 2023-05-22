c747985f37947b6b43177c3ca431cb9f2b811c87
Milyra35 <marie.richir35@gmail.com>
2023-05-22 09:05:29 +0000

    Début des cours Javascript

---
+++ res04-J1/exercice-boucles/index.js
@@ -0,0 +1,33 @@
+/**** EXERCICE 1 ****/
+let i = 0;
+while (i<11) {
+    console.log(i);
+    i = i + 1;
+}
+
+let x = 25;
+while (x>19) {
+    console.log(x);
+    x = x - 1;
+}
+
+let y = 0;
+while (y<21) {
+    console.log(y);
+    y = y + 2;
+}
+
+
+/**** EXERCICE 2 ****/
+let a = 0;
+while (a<11) {
+    console.log(a);
+    a = a + 1;
+    
+    if (a % 2) {
+        console.log("Pair");
+    }
+    else {
+        console.log("Impair");
+    }
+}
---
+++ res04-J1/exercice-variables/index.js
@@ -0,0 +1,21 @@
+const character = "Legolas";
+console.log(character);
+
+let name = "Legolas";
+name = "Gimli";
+console.log(name);
+
+let number = 14687;
+console.log(number);
+let decimal = 8.14783084763;
+console.log(decimal);
+let negative = -88;
+console.log(negative);
+
+let yes = true;
+let no = false;
+console.log(yes);
+console.log(no);
+
+let smallNumber = "34";
+console.log(number / smallNumber);
