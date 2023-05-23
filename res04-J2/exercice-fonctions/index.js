43ceb6e3fa3567691bb9d4f1d6c8298d671b318a
Milyra35 <marie.richir35@gmail.com>
2023-05-23 10:25:48 +0000

    Début du mini-projet en Javascript

--- res04-J2/exercice-fonctions/index.js
+++ res04-J2/exercice-fonctions/index.js
@@ -9,8 +9,8 @@ console.log(mod(42, 2)); // doit afficher 0
 
 
 function add(nb1, nb2) {
-    let sum = nb1 + nb2;
-    return sum;
+    let result = nb1 + nb2;
+    return result;
 }
 
 function sub(nb1, nb2) {
@@ -24,11 +24,13 @@ function mult(nb1, nb2) {
 }
 
 function div(nb1, nb2) {
+    let result = nb1 / nb2;
     if (nb2 === 0) {
         console.error("On ne peut pas diviser par 0");
     }
-    let result = nb1 / nb2;
-    return result;
+    else {
+        return result;
+    }
 }
 
 function mod(nb1, nb2) {
@@ -40,27 +42,52 @@ function mod(nb1, nb2) {
 
 // EXERCICE 2
 function askUserOperation() {
+    let operator = window.prompt("Choisis une opération");
     
-    if (operator !== "mod" || operator !== "div" || operator !== "mult" || operator !== "sub" || operator !== "add") {
+    if (operator === "%" || operator === "/" || operator === "*" || operator === "-" || operator === "+") {
+        return operator;
+    }
+    else {
         console.error("Ce n'est pas valide");
     }
-    
-    let operator = window.prompt("Choisis une opération");
-    return operator;
 }
 
 function askUserFirstNumber() {
-    let firstNumber = Number(window.prompt("Choisis un nombre"));
+    let firstNumber = parseInt(window.prompt("Choisis un nombre"));
     return firstNumber;
 }
 
 function askUserSecondNumber() {
-    let secondNumber = Number(window.prompt("Choisis un second nombre"));
+    let secondNumber = parseInt(window.prompt("Choisis un second nombre"));
     return secondNumber;
 }
 
 
 // EXERCICE 3
-function calculator(nb1, nb2, operator) {
+function calculator(operator, nb1, nb2) {
+    operator = askUserOperation();
+    nb1 = askUserFirstNumber();
+    nb2 = askUserSecondNumber();
+    let result;
     
+    if (operator === "+") {
+        result = add(nb1, nb2);
+    }
+    else if (operator === "-") {
+        result = sub(nb1, nb2);
+    }
+    else if (operator === "*") {
+        result = mult(nb1, nb2);
+    }
+    else if (operator === "/") {
+        result = div(nb1, nb2);
+    }
+    else if (operator === "%") {
+        result = mod(nb1, nb2);
+    }
+    
+    console.log(`${nb1} ${operator} ${nb2} = ${result}`);
 }
+
+calculator();
+
