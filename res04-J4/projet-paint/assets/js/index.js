246836eebb923d40af66d8137e5e8e4b6290b67f
Milyra35 <marie.richir35@gmail.com>
2023-05-25 13:32:39 +0000

    suite paint

--- res04-J4/projet-paint/assets/js/index.js
+++ res04-J4/projet-paint/assets/js/index.js
@@ -1,9 +1,11 @@
 function createGrid(rows, cols) {
     let scene = document.getElementById("scene");
+    scene.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
     
     
     for (let i=0; i<rows; i++) {
         let row = document.createElement("div");
+        row.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
         
         for (let j=0; j<cols; j++) {
             let col = document.createElement("div");
@@ -21,10 +23,33 @@ function selectBox() {
     for (box of boxes) {
         
         box.addEventListener("click", function(event) {
-            // box.classList.remove("selected");
+            for (let otherBox of boxes) {
+                otherBox.classList.remove("selected");
+            }
             let click = event.target;
             click.classList.toggle("selected");
-            
+        });
+    }
+}
+
+
+function changeColor() {
+    let colors = document.querySelector("header menu input");
+    
+    colors.addEventListener("change", function(event) {
+        let click = event.target.value;
+        sessionStorage.setItem("color", click);
+        console.log(click);
+    });
+    
+    
+    let boxes = document.querySelectorAll("main > div div");
+    
+    for (box of boxes) {
+        box.addEventListener("click", function(event) {
+            let actualColor = sessionStorage.getItem("color");
+            let click = event.target;
+            click.style.backgroundColor = actualColor;
         });
     }
 }
@@ -33,6 +58,7 @@ function selectBox() {
 
 
 window.addEventListener("DOMContentLoaded", function(){
-    createGrid(10, 10);
+    createGrid(30, 30);
     selectBox();
+    changeColor();
 });
